071622078a22c05888703b763aa4a337c004e5dd
Chetan Meshram <bash@Chetans-MacBook-Pro.local>
2017-07-18 07:24:36 -0700

    initial commit

---
+++ index.js
@@ -0,0 +1,7 @@
+var BasicCard = require("./basicCard");
+
+var firstPresident = new BasicCard(
+    "Who was the first president of the United States?", "George Washington");
+
+console.log(firstPresident.front);
+
