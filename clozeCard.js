a9f9cda4d73a8b81ca3307d2496e2de017d63e09
Chetan Meshram <bash@Chetans-MacBook-Pro.local>
2017-07-18 07:43:16 -0700

    export basicCard module

--- clozeCard.js
+++ clozeCard.js
@@ -1,1 +1,7 @@
+var ClozeCard = function(text, cloze) {
+    this.fullText = text;
+    this.cloze = cloze;
+    this.partial = text;
+}
+
 module.exports = ClozeCard;
--- index.js
+++ index.js
@@ -3,5 +3,9 @@ var BasicCard = require("./basicCard");
 var firstPresident = new BasicCard(
     "Who was the first president of the United States?", "George Washington");
 
-console.log(firstPresident.front);
+// "Who was the first president of the United States?"
+console.log(firstPresident.front); 
+
+// "George Washington"
+console.log(firstPresident.back); 
 
